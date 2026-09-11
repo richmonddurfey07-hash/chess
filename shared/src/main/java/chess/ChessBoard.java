9207de029c8640d18845ffbd0184006fd8061147
Richmond Durfey <rbd34@byu.edu>
2026-09-10 18:36:14 -0600

    impl (p0) basic move functionality

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,10 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    ChessPiece[][] squares = new ChessPiece[8][8];
 
     public ChessBoard() {
-        
+
     }
 
     /**
@@ -19,7 +20,8 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+
+        squares[position.getColumn()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +32,8 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+
+        return squares[position.getColumn()-1][position.getColumn()-1];
     }
 
     /**
