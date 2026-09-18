a24278e49c2eb606afe2976788e900fdbcc7c4ee
Richmond Durfey <rbd34@byu.edu>
2026-09-17 20:01:44 -0600

    Everything passes!!

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,12 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Collection;
+
+import static chess.ChessGame.TeamColor.*;
+import static chess.ChessPiece.PieceType.*;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,6 +15,27 @@ package chess;
  */
 public class ChessBoard {
     ChessPiece[][] squares = new ChessPiece[8][8];
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "squares=" + Arrays.toString(squares) +
+                '}';
+    }
 
     public ChessBoard() {
 
@@ -41,6 +69,61 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for(int i =1; i <=8; i++){
+            for(int j=1; j<=8; j++){
+                ChessPosition location = new ChessPosition(i,j);
+                addPiece(location,null);
+            }
+        }
+        for(int i = 1; i<= 8; i++){
+            ChessPosition BlackPosition = new ChessPosition(7,i);
+            ChessPiece black = new ChessPiece(BLACK,PAWN);
+            addPiece(BlackPosition,black);
+            ChessPosition WhitePosition = new ChessPosition(2,i);
+            ChessPiece white = new ChessPiece(WHITE,PAWN);
+            addPiece(WhitePosition,white);
+        }
+        ChessPosition BlackRook = new ChessPosition(8,1);
+        ChessPosition BlackKnight = new ChessPosition(8,2);
+        ChessPosition BlackBishop = new ChessPosition(8,3);
+        ChessPosition BlackQueen = new ChessPosition(8,4);
+        ChessPosition BlackKing = new ChessPosition(8,5);
+        ChessPosition BlackBishop2 = new ChessPosition(8,6);
+        ChessPosition BlackKnight2 = new ChessPosition(8,7);
+        ChessPosition BlackRook2 = new ChessPosition(8,8);
+        ChessPosition WhiteRook = new ChessPosition(1,1);
+        ChessPosition WhiteKnight = new ChessPosition(1,2);
+        ChessPosition WhiteBishop = new ChessPosition(1,3);
+        ChessPosition WhiteQueen = new ChessPosition(1,4);
+        ChessPosition WhiteKing = new ChessPosition(1,5);
+        ChessPosition WhiteBishop2 = new ChessPosition(1,6);
+        ChessPosition WhiteKnight2 = new ChessPosition(1,7);
+        ChessPosition WhiteRook2 = new ChessPosition(1,8);
+        ChessPiece blackRook = new ChessPiece(BLACK,ROOK);
+        ChessPiece blackKnight = new ChessPiece(BLACK,KNIGHT);
+        ChessPiece blackBishop = new ChessPiece(BLACK,BISHOP);
+        ChessPiece blackKing = new ChessPiece(BLACK,KING);
+        ChessPiece blackQueen = new ChessPiece(BLACK,QUEEN);
+        ChessPiece whiteRook = new ChessPiece(WHITE,ROOK);
+        ChessPiece whiteKnight = new ChessPiece(WHITE,KNIGHT);
+        ChessPiece whiteBishop = new ChessPiece(WHITE,BISHOP);
+        ChessPiece whiteKing = new ChessPiece(WHITE,KING);
+        ChessPiece whiteQueen = new ChessPiece(WHITE,QUEEN);
+        addPiece(BlackRook,blackRook);
+        addPiece(BlackRook2, blackRook);
+        addPiece(BlackKnight,blackKnight);
+        addPiece(BlackKnight2, blackKnight);
+        addPiece(BlackBishop,blackBishop);
+        addPiece(BlackBishop2, blackBishop);
+        addPiece(BlackQueen,blackQueen);
+        addPiece(BlackKing, blackKing);
+        addPiece(WhiteRook,whiteRook);
+        addPiece(WhiteRook2, whiteRook);
+        addPiece(WhiteKnight,whiteKnight);
+        addPiece(WhiteKnight2, whiteKnight);
+        addPiece(WhiteBishop,whiteBishop);
+        addPiece(WhiteBishop2, whiteBishop);
+        addPiece(WhiteKing,whiteKing);
+        addPiece(WhiteQueen, whiteQueen);
     }
 }
